ba63cc63aba8ef9f672df2e53c14f83b835f0872
kusmiderdaniel <kusmiderdaniel@gmail.com>
2025-06-25 09:38:52 +0200

    unified versioning

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 
-const APP_VERSION = '2.25062025f-dev';
+const APP_VERSION = '2.25062025a';
 
 const AppFooter: React.FC = () => {
   const { user } = useAuth();
