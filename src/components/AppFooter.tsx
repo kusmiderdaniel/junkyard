a51697ee5d9a066d9fffa6e1b5f9bcb8480497e6
GitHub Action <action@github.com>
2025-06-25 05:26:32 +0000

    🔖 Auto-version: 2.25062025a-dev

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 
-const APP_VERSION = '2.25062025-dev';
+const APP_VERSION = '2.25062025a-dev';
 
 const AppFooter: React.FC = () => {
   const { user } = useAuth();
