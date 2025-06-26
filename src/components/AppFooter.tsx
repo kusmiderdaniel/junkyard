96c7fe003db8b7ece75b8ce57f7aa4ee076d7dfd
GitHub Action <action@github.com>
2025-06-26 07:35:18 +0000

    🔖 Auto-version: 2.26062025

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 
-const APP_VERSION = '2.25062025h';
+const APP_VERSION = '2.26062025';
 
 const AppFooter: React.FC = () => {
   const { user } = useAuth();
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,7 @@ import { auth } from '../firebase';
 import { RateLimitedAuth } from '../utils/rateLimitedFirebase';
 import AuthDebug from './AuthDebug';
 
-const APP_VERSION = '2.25062025h';
+const APP_VERSION = '2.26062025';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
