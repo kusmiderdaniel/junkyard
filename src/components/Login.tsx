adb861b6ec24063b745123d5f38fa911fb60200e
GitHub Action <action@github.com>
2025-07-01 18:37:25 +0000

    🔖 Auto-version: 2.01072025f

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,7 @@ import { auth } from '../firebase';
 import { RateLimitedAuth } from '../utils/rateLimitedFirebase';
 import AuthDebug from './AuthDebug';
 
-const APP_VERSION = '2.01072025e';
+const APP_VERSION = '2.01072025f';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
